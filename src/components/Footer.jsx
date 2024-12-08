ca4d79abbf7745c7ce32d1e6dd8e626a5ad0f9df
ammar-bin-quddus <ammarcghs372@gmail.com>
2024-12-08 22:49:40 +0600

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,23 +6,24 @@ const Footer = () => {
       <footer className="footer bg-neutral text-neutral-content p-10">
         <nav>
           <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
+          <a className="link link-hover">Campaign Creation</a>
+          <a className="link link-hover">Campaign Management</a>
+          <a className="link link-hover">Fundraising Guidance</a>
+          <a className="link link-hover">Donor Management</a>
         </nav>
         <nav>
           <h6 className="footer-title">Company</h6>
           <a className="link link-hover">About us</a>
           <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <a className="link link-hover">Our Mission</a>
+          <a className="link link-hover">Careers</a>
         </nav>
         <nav>
           <h6 className="footer-title">Legal</h6>
           <a className="link link-hover">Terms of use</a>
           <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
+          <a className="link link-hover">Cookie Policy</a>
+          <a className="link link-hover">Refund Policy</a>
         </nav>
       </footer>
     </div>
